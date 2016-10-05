11e778e44159ec4c9d8682bc1aca4c724cef9266
Manash Kumar Mandal <manash@protonmail.com>
2016-10-05 21:45:02 +0600

    update

--- Day2/AVR_WITH_HC05/AVR_with_BT_sendstring/AVR_with_BT_sendstring/main.cpp
+++ Day2/AVR_WITH_HC05/AVR_with_BT_sendstring/AVR_with_BT_sendstring/main.cpp
@@ -20,10 +20,13 @@
 
 #define MYUBRR F_CPU/16/BAUD - 1
 
+#include <avr/sfr_defs.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include <string.h>
 #include <stdlib.h>
+
+typedef uint8_t uint;
 
 void USART_Init(unsigned int ubrr)
 {
@@ -50,26 +53,48 @@ bool USART_Transmit_String(char* command)
 	return ok;
 }
 
+bool USART_Transmit_With_CRNL(char *command){
+	uint length = strlen(command);
+	char *string_with_flags = (char*) malloc(sizeof(char) * (length + 10));
+	for (uint i = 0; i < length; i++){
+		string_with_flags[i] = command[i];
+	}
+	string_with_flags[length]     = '\r';
+	string_with_flags[length + 1] = '\n';
+	string_with_flags[length + 2] = '\0';
+	
+	USART_Transmit_String(string_with_flags);
+	
+	free (string_with_flags); //Free the memory 
+}
+
 unsigned char USART_Receive(void)
 {
 	while (! (UCSRA & (1 << RXC)));
 	return UDR;
 }
 
+uint8_t receiveByte(void){
+	loop_until_bit_is_clear(UCSRA, RXC);
+	return UDR;
+}
+
 char *charToString(unsigned char in){
-	char *out = (char*) malloc(sizeof(char) * 3);
+	char *out = (char*) malloc(sizeof(char) * 4);
 	out[0] = in;
-	out[1] = '\n';
-	out[2] = '\0';
+	out[1] = '\r';
+	out[2] = '\n';
+	out[3] = '\0';
 	return out;
 }
 
 
+
 int main(void){
 	USART_Init(MYUBRR);
-	char *strtosend = "Hello World\r\n";
+	char* strtosend_ = "Hello AVR Enthusiasts!";
 	while(1){
-		USART_Transmit_String(strtosend);
+		USART_Transmit_With_CRNL(strtosend_);
 		_delay_ms(10);
 	}
 }
