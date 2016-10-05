f274746fa965268c5001461a1373ff326fb9f6e6
Manash Kumar Mandal <manash@protonmail.com>
2016-10-05 20:16:04 +0600

    Add files via upload

---
+++ Day2/UART_SIMULTANEOUS_RX_TX/RX_TX_AVR/RX_TX_AVR.ino
@@ -0,0 +1,22 @@
+#define BAUD 9600
+
+void setup(){
+  Serial.begin(BAUD);
+  Serial1.begin(BAUD); //Connect AVR to these pair of RX TX
+  Serial.println("Initialization");
+}
+
+void loop(){
+  Serial1.print('a');
+  delay(500);
+}
+
+
+void serialEvent1(){
+  String b;
+  while (Serial1.available() > 0){
+    b = Serial1.readStringUntil('\n');
+    Serial.println(b);
+  }
+}
+
