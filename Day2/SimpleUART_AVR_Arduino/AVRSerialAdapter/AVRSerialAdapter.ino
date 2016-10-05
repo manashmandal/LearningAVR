ee9b1fcc0e84573d0bbc6ff340279597980cd4db
Manash Kumar Mandal <manash@protonmail.com>
2016-10-05 14:46:57 +0600

    Arduino Receiver Code

---
+++ Day2/SimpleUART_AVR_Arduino/AVRSerialAdapter/AVRSerialAdapter.ino
@@ -0,0 +1,21 @@
+#define BAUD 9600
+#define LED 13
+
+char receivedChar;
+
+void setup(){
+  Serial.begin(BAUD);
+  Serial1.begin(BAUD); //Connect AVR to these pair of RX TX
+  Serial.println("Initialization");
+  pinMode(LED, OUTPUT);
+}
+
+void loop(){
+  if ((receivedChar = Serial1.read()) > 0){
+    Serial.println(receivedChar);
+    digitalWrite(LED, HIGH);
+  } else {
+    digitalWrite(LED, LOW);
+  }
+}
+
