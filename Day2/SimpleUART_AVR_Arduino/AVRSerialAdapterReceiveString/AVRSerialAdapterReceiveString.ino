83301f1971b77078462725d6d16a7e506ced5b5e
Manash Kumar Mandal <manash@protonmail.com>
2016-10-05 15:27:06 +0600

    Now receives string

---
+++ Day2/SimpleUART_AVR_Arduino/AVRSerialAdapterReceiveString/AVRSerialAdapterReceiveString.ino
@@ -0,0 +1,14 @@
+#define BAUD 9600
+
+void setup(){
+  Serial.begin(BAUD);
+  Serial1.begin(BAUD); //Connect AVR to these pair of RX TX
+  Serial.println("Initialization");
+}
+
+void loop(){
+  if (Serial1.available() > 0){
+    Serial.println(Serial1.readStringUntil('\n'));
+  }
+}
+
