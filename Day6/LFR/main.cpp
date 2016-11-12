404d7b562861263907cb1b8d54f87b266db9c6f2
Manash Kumar Mandal <manash@protonmail.com>
2016-11-13 01:55:27 +0600

    Update main.cpp

--- Day6/LFR/main.cpp
+++ Day6/LFR/main.cpp
@@ -1,17 +1,3 @@
-/*
- * LFR.cpp
- *
- * Created: 11/10/2016 2:07:21 PM
- * Author : Manash
- */ 
-
-
-//#define F_CPU 16000000UL
-
-//#define BAUD 9600
-
-//#define MYUBRR F_CPU/16/BAUD - 1
-
 #include <atmega8/Serial.h>
 #include <avr/io.h>
 #include <util/delay.h>
@@ -35,15 +21,21 @@ uint16_t analogRead(uint8_t channel);
 	
 int getPosition(void){
 	int totalValue = 0;
+	int num_active_sensor = 0;
 	for (int i = 0; i < NUM_SENSOR; i++){
 		if (analogRead(sensors[i]) < threshold){
 			sensor_value[i] = ON_LINE;
+			num_active_sensor++;
 		} else {
 			sensor_value[i] = OFF_LINE;
 		}
 		totalValue += weights[i] * sensor_value[i];
 	}
-	return (totalValue / NUM_SENSOR);
+	
+	if (num_active_sensor == 0)
+		return 0;
+	else 
+		return (totalValue / num_active_sensor);
 }
 
 void debugSensor(int pos, int val){
@@ -106,32 +98,6 @@ void backward(unsigned int ls, unsigned int rs){
 	rightMotorBackward(rs);
 }
 
-/*
-void USART_Init(unsigned int ubrr)
-{
-	UBRRH = (unsigned char) (ubrr >> 8);
-	UBRRL = (unsigned char) ubrr;
-	
-	UCSRB = (1 << RXEN) | (1 << TXEN);
-	UCSRC = (1 << URSEL) | (1 << USBS) | (3 <<UCSZ0);
-	
-}
-
-void USART_Transmit(unsigned char data){
-	while (!(UCSRA & (1 << UDRE))) {};
-	UDR = data;
-}
-
-bool USART_Transmit_String(char* command)
-{
-	bool ok = false;
-	for (int i = 0; i < strlen(command); i++){
-		USART_Transmit(command[i]);
-		ok = true;
-	}
-	return ok;
-}
-*/
 
 static inline void initPWM(void){
 	// Selecting PWM Mode [FastPWM - 8bit]
@@ -147,78 +113,9 @@ static inline void initPWM(void){
 }
 
 
-/*
-int main(void){
-	USART_Init(MYUBRR);
-	char command[] = "Hello World!\r\n";
-	while(1){
-		USART_Transmit_String(command);
-		_delay_ms(1000);
-	}
-}
-*/
-
-
-/*
-int main(void){
-	initAllOut();
-	while(true){
-		blinkAll();
-	}
-}*/
-
-/*
-int main(void){
-	initPWM();
-	
-	while(true){
-		for (int i = 0; i < 255; i++){
-			OCR1A = i;
-			OCR1B = i;
-			_delay_ms(10);
-		}
-		_delay_ms(1000);
-	}
-}
-*/
-
 void followLine(void){
 	int position = getPosition();
 	USART_Transmit_Number_With_CRNL(position);
-	
-	////HARD RIGHT
-	//if (position > 100 && position < 200){
-		//forward(180, 165);
-	//} 
-	//// SOFT RIGHT
-	//else if (position > 200 && position <= 500){
-		//forward(170, 165);
-	//}
-	//
-	//// HARD LEFT
-	//else if (position >= 2000 && position < 3000){
-		//forward(165, 180);
-	//}
-	//
-	//// SOFT LEFT
-	//else if (position > 1700 && position < 2000) {
-		//forward(165, 170);
-	//}
-	//
-	//// FORWARD
-	//else if (position >= 1000 && position <= 1700){
-		//forward(165, 165);
-	//}
-	//
-	//else {
-		//forward(0, 0);
-	//}
-	
-	if (position >= 1000 && position <= 1666){
-		forward(150, 150);
-	} else {
-		forward(0, 0);
-	}
 }
 
 int main(){
@@ -229,18 +126,6 @@ int main(){
 	char x[] = "Hello world\r\n";
 	USART_Transmit_String(x);
 	while(1){
-		//forward(150, 165);
-	USART_Transmit_With_CRNL("----------\n\n");
-	for (int i = 0; i < NUM_SENSOR; i++){
-		debugSensor(i, analogRead(sensors[i]));
-		_delay_ms(100);	
-	}
-		//debugSensor(0, getPosition());
-		//followLine();
-		//USART_Transmit_With_CRNL("----------\n\n");
-		//USART_Transmit_Number_With_CRNL(analogRead(6));
-		_delay_ms(1000);
-		
 		
 	}
 }
